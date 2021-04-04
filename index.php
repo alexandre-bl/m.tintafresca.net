251cc31d9ab0a3c5be1b4fc62a82d0c9145396ca
Alexandre B. L <alexandrebl@protonmail.com>
2021-04-04 16:35:28 +0100

    04/04/2021 16:35:28

--- index.php
+++ index.php
@@ -1,15 +1,51 @@
+<?php
+
+    $title = get_bloginfo( 'name' );
+    $meta_img = get_site_icon_url();
+    if( is_single() ) {
+        if( !empty(wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0]) ) {
+            $meta_img = wp_get_attachment_image_src( get_post_thumbnail_id( $post->ID ), 'single-post-thumbnail' )[0];
+        }
+    }
+    $desc = get_bloginfo( 'description' );
+    $meta_desc = get_post_meta( get_post()->ID, "Description", TRUE );
+
+    if( is_singular() ) {
+
+        $title = get_the_title();
+
+        if( !empty( $meta_desc ) ) {
+
+            $desc = $meta_desc;
+
+        }
+
+    }
+
+?>
+
 <!DOCTYPE html>
 <html>
 
     <head>
 
+        <title><?php echo $title; ?></title>
+        <meta name="description" content="<?php echo $desc; ?>">
+        <meta name="og:image" content="<?php echo $meta_img; ?>">
+        <link rel="icon" href="<?php echo get_site_icon_url(); ?>">
+
         <link rel='stylesheet' href='<?php echo get_template_directory_uri()."/style.css"; ?> '>
 
+        <meta charset="utf-8">
+        
     </head>
 
     <body>
 
         <div id="header">
+
+            <h1 id="title"> 
+
         </div>
 
     </body>
