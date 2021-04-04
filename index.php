b7930bb39a8ae4823f92e48be1bc4d3d7d24fa80
Alexandre B. L <alexandrebl@protonmail.com>
2021-04-04 18:38:55 +0100

    04/04/2021 18:38:55

--- index.php
+++ index.php
@@ -2,6 +2,8 @@
 
     require_once __DIR__ . "/handy.php";
 
+    $logo_id = get_theme_mod( 'custom_logo' );
+    $logo = wp_get_attachment_image_src( $logo_id , 'full' )[0];
     $editor  = get_users( [ 'role__in' => [ 'editor' ] ] )[0]->display_name;
     $title = get_bloginfo( 'name' );
     $meta_img = get_site_icon_url();
@@ -47,7 +49,7 @@
 
         <div id="header">
 
-            <img id="icon" src="<?php echo get_site_icon_url(); ?>">
+            <img id="icon" src="<?php echo $logo; ?>">
 
             <p  id="edition"> Edição: <?php echo get_edition(); ?> </p>
 
