c2a33f38b7751f9e8ddc52ee7a5b3ccdf4122c89
Alexandre B. L <alexandrebl@protonmail.com>
2021-04-05 17:45:11 +0100

    05/04/2021 17:45:11

--- site_front/singular.php
+++ site_front/singular.php
@@ -5,12 +5,17 @@
         $post = get_post();
 
         $content = $post->post_content;
+        $date    = $post->post_date;
 
     ?>
 
     <h1 id="post_title"><?php
         echo $sing_title;
     ?></h1>
+
+    <h2 class='post_date'> <?php
+        echo $date;
+    ?> </h2>
 
     <div id="post_content"><?php
         echo apply_filters( 'the_content',  $content );
