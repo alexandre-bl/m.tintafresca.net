5177833ce8df6196cef73d356e73ea25461eb121
Alexandre B. L <alexandrebl@protonmail.com>
2021-04-05 16:34:47 +0100

    05/04/2021 16:34:47

--- site_front/header.php
+++ site_front/header.php
@@ -8,6 +8,18 @@
 
     </a>
 
-    <img id="menu_button" src="<?php echo get_template_directory_uri()."/hamburger.png"; ?>">
+    <a href="<?php
+
+        echo get_site_url();
+
+        if( $page != "pages" ) {
+
+            echo "?page='pages'";
+
+        }
+
+    ?>">
+        <img id="menu_button" src="<?php echo get_template_directory_uri()."/hamburger.png"; ?>">
+    </a>
 
 </div>
