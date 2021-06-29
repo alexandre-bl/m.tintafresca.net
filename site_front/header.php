df2e113e6bab2201e8b09df1ab778f2a361d77fe
Alexandre B. L <alexandrebl@protonmail.com>
2021-06-29 18:54:33 +0100

    29/06/2021 18:54:33

--- site_front/header.php
+++ site_front/header.php
@@ -1,3 +1,22 @@
+<?php
+
+global $wpdb;
+$table_name = "adds";
+$table_name = $wpdb->prefix . $table_name;
+$charset_collate = $wpdb->get_charset_collate();
+require_once(ABSPATH . "wp-admin/includes/upgrade.php");
+
+dbDelta(" CREATE TABLE IF NOT EXISTS $table_name ( img TEXT, link TEXT ) $charset_collate; ");
+
+$query = $wpdb->get_results(" SELECT * FROM $table_name ");
+
+$add = array( "img" => "", "url" => "" );
+
+if( !empty( $query[0] ) ) { $add["img"]  = str_replace( "http://tintafresca.net", "https://tintafresca.net", $query[0]->img );
+                            $add["url"] = str_replace( "http://tintafresca.net", "https://tintafresca.net", $query[0]->link); }
+
+?>
+
 <div id="header">
 
     <a href="<?php echo get_site_url(); ?>">
@@ -28,3 +47,4 @@
     </a>
 
 </div>
+<a class="add" id="header_add" href="<?php echo $add["url"]; ?>"> <img src="<?php echo $add["img"]; ?>"> </a>
